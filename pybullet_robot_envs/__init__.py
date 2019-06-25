4dad7c4e48e79a41a89f0ebb463222f7eaf82f03
erampone <elena.rampone@iit.it>
2019-06-25 14:42:44 +0200

    fix envs registration problems after merge

--- pybullet_robot_envs/__init__.py
+++ pybullet_robot_envs/__init__.py
@@ -6,14 +6,14 @@ from gym.envs.registration import register
 
 register(
         id='iCubReach-v0',
-        entry_point='pybullet_robot_envs.envs.icub_envs:iCubReachGymEnv',
+        entry_point='pybullet_robot_envs.envs:iCubReachGymEnv',
         max_episode_steps=1000,
         kwargs={ 'useIK':1, 'isDiscrete':0, 'control_arm':'l', 'useOrientation':0, 'rnd_obj_pose':1, 'maxSteps':1000},
 )
 
 register(
         id='iCubPush-v0',
-        entry_point='pybullet_robot_envs.envs.icub_envs:iCubPushGymEnv',
+        entry_point='pybullet_robot_envs.envs:iCubPushGymEnv',
         max_episode_steps=1000,
         kwargs={ 'useIK':1, 'isDiscrete':0, 'control_arm':'l', 'useOrientation':0, 'rnd_obj_pose':1, 'maxSteps':1000},
 )
@@ -21,7 +21,7 @@ register(
 
 register(
         id='pandaReach-v0',
-        entry_point='pybullet_robot_envs.envs.panda_envs:pandaReachGymEnv',
+        entry_point='pybullet_robot_envs.envs:pandaReachGymEnv',
         max_episode_steps=1000,
         kwargs={
                  'useIK':0,
