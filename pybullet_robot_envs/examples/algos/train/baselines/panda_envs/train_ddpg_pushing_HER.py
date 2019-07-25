2fadcf8776820d2b3cecb71dbdf2dfe2ed83fa82
Andrea Romdhana <andreronda@macbookdiandrea.iit.local>
2019-07-25 09:58:02 +0200

    minox fix

--- pybullet_robot_envs/examples/algos/train/baselines/panda_envs/train_ddpg_pushing_HER.py
+++ pybullet_robot_envs/examples/algos/train/baselines/panda_envs/train_ddpg_pushing_HER.py
@@ -22,7 +22,7 @@ import numpy as np
 class CustomPolicy(FeedForwardPolicy):
     def __init__(self, *args, **kwargs):
         super(CustomPolicy, self).__init__(*args, **kwargs,
-                                           layers=[256,256,256],
+                                           layers=[256,128,128],
                                            layer_norm=False,
                                            act_fun=tf.nn.relu,
                                            feature_extraction="lnmlp")
@@ -45,7 +45,7 @@ def main():
     # -r
     normalize_returns = True
     # -t
-    timesteps = 10000000
+    timesteps = 2000000
     policy_name = "pushing_policy"
     discreteAction = 0
     rend = False
@@ -53,21 +53,20 @@ def main():
             isDiscrete=discreteAction, numControlledJoints = numControlledJoints,
             fixedPositionObj = fixed, includeVelObs = True)
 
-    env = SubprocVecEnv([lambda: env for i in range(3)])
-
     # Available strategies (cf paper): future, final, episode, random
     goal_selection_strategy = 'future' # equivalent to GoalSelectionStrategy.FUTURE
     n_actions = env.action_space.shape[-1]
     action_noise = OrnsteinUhlenbeckActionNoise(mean=np.zeros(n_actions), sigma=float(0.5) * np.ones(n_actions))
     # Wrap the model
 
-    model = HER(CustomPolicy, env, model_class, n_sampled_goal=4, goal_selection_strategy=goal_selection_strategy,
-                verbose=1,tensorboard_log="../pybullet_logs/panda_push_ddpg/stable_baselines/DDPG+HER_RANDOM", buffer_size=1000000,batch_size=256,
+    model = HER(CustomPolicy, env, model_class, n_sampled_goal=8, goal_selection_strategy=goal_selection_strategy,
+                verbose=1,tensorboard_log="../pybullet_logs/panda_push_ddpg/stable_baselines/DDPG+HER_RANDOM_PHASES", buffer_size=1000000,batch_size=256,
                 random_exploration=0.3, action_noise=action_noise)
     # Train the model
-    #model = HER.load("../policies/pushing_fixed_HER.pkl", env=env)
-    model.learn(timesteps)
-    model.save("../policies/pushing_random_HER.pkl")
+    for i in range(5):
+        model.learn(timesteps)
+        model.save("../policies/pushing_random_HER"+str(i))
+        model = HER.load("../policies/pushing_random_HER"+str(i), env=env)
 
 if __name__ == "__main__":
     main()
