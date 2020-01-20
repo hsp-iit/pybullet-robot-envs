f5beaac75a5d248457b3ce7737f306e16204fbf5
erampone <elena.rampone@iit.it>
2020-01-20 12:11:42 +0100

    update gitignore and license tag

--- setup.py
+++ setup.py
@@ -29,6 +29,7 @@ setup(
     author="Elena Rampone, Andrea Romdhana, Giulia Vezzani",
     author_email="elena.rampone@iit.it, andrea.romdhana@iit.it, giulia.vezzani@iit.it",
     description="PyBullet Robot Envs: A collection of OpenAI Gym RL robotic environments in PyBullet simulator",
+    license="LGPL",
     url='https://github.com/robotology-playground/pybullet-robot-envs',
     python_requires='>=3.5',
     #install_requires=['gym==0.12.5','pybullet==2.5.0'],
