127d29ada50eeb15ba756d487a08ff9117fa645c
Elena Rampone <elena.rampone18@gmail.com>
2020-06-02 10:38:02 +0200

    gym envs registration

--- pybullet_robot_envs/envs/__init__.py
+++ pybullet_robot_envs/envs/__init__.py
@@ -1,5 +1,7 @@
 from pybullet_robot_envs.envs.icub_envs.icub_reach_gym_env import iCubReachGymEnv
 from pybullet_robot_envs.envs.icub_envs.icub_push_gym_env import iCubPushGymEnv
+from pybullet_robot_envs.envs.icub_envs.icub_push_gym_goal_env import iCubPushGymGoalEnv
 
 from pybullet_robot_envs.envs.panda_envs.panda_reach_gym_env import pandaReachGymEnv
 from pybullet_robot_envs.envs.panda_envs.panda_push_gym_env import pandaPushGymEnv
+from pybullet_robot_envs.envs.panda_envs.panda_push_gym_goal_env import pandaPushGymGoalEnv
